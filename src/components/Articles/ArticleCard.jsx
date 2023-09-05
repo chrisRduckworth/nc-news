5f034b6ae81e85f0f78ff51102bd0f5a34bbd953
chrisRduckworth <c_duckworth@hotmail.com>
2023-09-05 13:48:35 +0100

    add some styling

--- src/components/Articles/ArticleCard.jsx
+++ src/components/Articles/ArticleCard.jsx
@@ -12,22 +12,23 @@ function ArticleCard({ article }) {
     votes,
   } = article;
   return (
-    <div className="article-card">
-      <Link to={`/articles/${article_id}`} className="article-card-title">
-        <h3>{title}</h3>
-      </Link>
+    // <div className="article-card">
+      <Link to={`/articles/${article_id}`} className="article-card">
+        <h3 className="article-card-title">{title}</h3>
+      
       <img
         className="article-thumbnail"
         src={article_img_url}
         alt={`${title} article thumbnail`}
       />
       <p className="article-card-topic">{topic}</p>
-      <p className="article-card-rating">{votes} votes</p>
+      <p className="article-card-rating">{votes} vote{votes !== 1 && "s"}</p>
       <p className="article-card-author">{author}</p>
       <p className="article-card-date">
         {dayjs(created_at).format("DD/MM/YYYY")}
       </p>
-    </div>
+      </Link>
+    // </div>
   );
 }
 
