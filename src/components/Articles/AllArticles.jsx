9709931cfded76ad870c8f65be33f6d36a2fa0fa
chrisRduckworth <c_duckworth@hotmail.com>
2023-09-06 09:47:22 +0100

    add error handling for pagination

--- src/components/Articles/AllArticles.jsx
+++ src/components/Articles/AllArticles.jsx
@@ -7,27 +7,52 @@ import "./Articles.css";
 import { useSearchParams } from "react-router-dom";
 
 // TO DO:
-// figure out why it's rendering so many bloody times
-// do ali's optimistic rendering
+// figure out why it's rendering so many bloody times DONE but i don't know how to fix it
+// it's because every time state changes it rerenders, including when isLoading etc
+// do ali's optimistic rendering DONE
 // get categories to work
-// error for page = 10. or redirect?
+// error for page = 10. or redirect? DONE
 
 function AllArticles() {
-  const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [searchParams, setSearchParams] = useSearchParams();
 
-  if (!searchParams.get("p")) searchParams.set("p", 1);
+  console.log("rendering")
 
   useEffect(() => {
     setIsLoading(true);
-    getArticles({ p: searchParams.get("p") }).then((articles) => {
-      setIsLoading(false);
-      setArticles(articles);
-    });
+    getArticles({ p: searchParams.get("p")})
+      .then((articles) => {
+        const lastPage = Math.ceil(articles.total_count / 10) || 1
+        if (searchParams.get("p") > lastPage) {
+          console.log("in if")
+          setSearchParams((currParams) => {
+            currParams.set("p", lastPage);
+            return currParams;
+          });
+        } else if (searchParams.get("p") <= 0) {
+          console.log("in else if")
+          setSearchParams((currParams) => {
+            currParams.set("p", 1);
+            return currParams;
+          });
+        } else {
+          console.log("articles rendering")
+          setIsLoading(false);
+          setArticles(articles);
+        }
+      })
+      .catch((err) => {
+        if (err.response.data.msg === "Invalid Page") {
+          console.log("in error")
+          setSearchParams((currParams) => {
+            currParams.set("p", 1);
+            return currParams;
+          });
+        }
+      });
   }, [searchParams]);
-
-  console.log(searchParams.get("p"));
 
   return (
     <main>
