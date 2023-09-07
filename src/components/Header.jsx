c6b05d8b54a62f3c971e2c9c27e086c3e74ebb95
chrisRduckworth <c_duckworth@hotmail.com>
2023-09-07 15:07:23 +0100

    style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,51 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { UserContext } from "../contexts/User";
+import { getTopics } from "../../utils/api";
 
 function Header() {
   const { user } = useContext(UserContext);
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    getTopics().then((topicData) => setTopics(topicData));
+  }, []);
+
   return (
     <header>
       <div id="header-container">
         <h1>Northcoders News</h1>
-        <div id="header-buttons">
-          <button>Dark Mode</button>
-          {user}
-          <Link to="/login">{user ? "change user" : "login"}</Link>
-        </div>
+        <button>🌙</button>
       </div>
       <nav className="shortcuts">
-        <Link to="/articles">Articles</Link>
-        <Link to="/new_article">New Article</Link>
-        <Link to="/users">Users</Link>
-        {/* some sort of categories link as well */}
+        <div className="left-shortcuts">
+          <div className="dropdown">
+            <span className="header-link">Articles ↓</span>
+            <div className="dropdown-content">
+              <Link to="/articles">View all</Link>
+              {topics.map((topic) => {
+                return (
+                  <Link key={topic.slug} to={`/articles?topic=${topic.slug}`}>
+                    {topic.slug}
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+          <Link to="/new_article" className="header-link">
+            New Article
+          </Link>
+          <Link to="/users" className="header-link">
+            Users
+          </Link>
+        </div>
+        <div>
+          {user}
+          <Link to="/login" id="login-link">
+            {user ? "Change user" : "Login"}
+          </Link>
+        </div>
       </nav>
     </header>
   );
